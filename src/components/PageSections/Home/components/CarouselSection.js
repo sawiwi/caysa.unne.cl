c0b1e93e8e9e07eef3830a2a43d253e77d84b6d0
fabian <fabian.salas.astete@gmail.com>
2023-12-04 14:29:04 -0300

    issues: chamges on carousel, about me data

--- src/components/PageSections/Home/components/CarouselSection.js
+++ src/components/PageSections/Home/components/CarouselSection.js
@@ -4,7 +4,11 @@ import { mainCarouselData } from '../../../../data';
 
 const CarouselSection = () => (
   <div className="h-[400px] xl:h-[600px]" id='IndexSection'>
-    <MainCarousel data={mainCarouselData} />
+    <div className="grid col-span-1 xl:col-span-3 ">
+      <div className="object-cover w-[100%] h-[400px] xl:h-[500px]" >
+        <MainCarousel data={mainCarouselData} />
+      </div>
+      </div>
   </div>
 );
 
